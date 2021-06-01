2c77c10dbe98286ec846a5b1141c528f280ca1ac
Matrucci <matan.saloniko@gmail.com>
2021-06-01 20:07:45 +0300

    initla commit

---
+++ app/src/main/java/com/matt/flightgearcontrol/views/MainActivity.java
@@ -0,0 +1,16 @@
+package com.matt.flightgearcontrol.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.matt.flightgearcontrol.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
